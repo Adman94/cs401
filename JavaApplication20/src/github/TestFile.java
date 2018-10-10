34895c954a9bbae880c16eaf740cbac2516b8924
damir <damir@DESKTOP-4RE40R2>
2018-10-10 15:07:53 -0700

    commit change damirg

--- JavaApplication20/src/github/TestFile.java
+++ JavaApplication20/src/github/TestFile.java
@@ -12,6 +12,6 @@ package github;
 public class TestFile {
     public static void main(String[] args) {
         System.out.println("Test file to push");
-        // test
+        // test damirg
     }
 }
