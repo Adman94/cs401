245d8939f96ae90c135b15dce89fc17fed6f01d0
Adman94 <adman00794@gmail.com>
2018-10-10 13:43:54 -0700

    2nd commit

---
+++ JavaApplication20/src/github/TestFile.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github;
+
+/**
+ *
+ * @author habe
+ */
+public class TestFile {
+    public static void main(String[] args) {
+        System.out.println("Test file to push");
+    }
+}
